70069d05b5d0287d6741e0a3042ef752e5904dc9
김주영 <98079486+Leusin@users.noreply.github.com>
2025-03-20 15:48:49 +0900

    [수정] 선형 탐색 함수 매개변수 이름 수정 및 주석 추가

--- Search/LinearSearch/LinearSearch.cpp
+++ Search/LinearSearch/LinearSearch.cpp
@@ -1,25 +1,33 @@
 /*
  * 선형 탐색(Linear Search)
  *
- * 리스트에서 각 요소를 처음부터 끝까지 차례대로 비교하여 원하는 값을 찾는 
- * 알고리즘입니다. 
- * 
+ * 리스트에서 각 요소를 처음부터 끝까지 차례대로 비교하여 원하는 값을 찾는
+ * 알고리즘입니다.
+ *
  * 배열이 정렬되어 있지 않아도 사용할 수 있으며, 시간 복잡도는 O(N)으로
- * 입력 크기에 비례해 탐색 시간이 증가합니다. 
- * 
+ * 입력 크기에 비례해 탐색 시간이 증가합니다.
+ *
  * 공간 복잡도는 O(1)로 추가 메모리가 필요하지 않습니다. 데이터가 커지면
  * 비효율적일 수 있어, 주로 작은 데이터 집합에 사용됩니다.
+ * 
  */
 
 #include <iostream>
 
 using namespace std;
 
-int LinearSearch(int arr[], int size, int x)
+/**
+ * 선형 탐색 알고리즘 함수
+ * @param arr 탐색할 배열
+ * @param size 배열 크기
+ * @param target 찾고자 하는 값
+ * @return 배열에서 target 값의 인덱스, 없으면 -1 반환
+ */
+int LinearSearch(int arr[], int size, int target)
 {
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] == x)
+        if (arr[i] == target)
         {
             return i;
         }
