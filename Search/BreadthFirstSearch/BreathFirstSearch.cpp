e750ccc2c982fa972e516afe4a8a8ab286bb1380
김주영 <98079486+Leusin@users.noreply.github.com>
2025-03-21 20:35:53 +0900

    [수정] BFS 함수의 지역변수 이름 변경 및 주석 보강

--- Search/BreadthFirstSearch/BreathFirstSearch.cpp
+++ Search/BreadthFirstSearch/BreathFirstSearch.cpp
@@ -3,11 +3,12 @@
  *
  * 너비 우선 탐색(BFS)은 그래프를 순회하는 기본적인 알고리즘으로, 시작
  * 정점에서 가까운 정점부터 차례로 탐색합니다.
- *
+ * 
  * 큐(Queue)를 활용하여 정점을 방문하고, 방문한 정점의 이웃을 차례로
- * 큐에 넣으며 진행하며, 방문 순서는 계층적(Level-wise)으로 확장됩니다.
- *
- * 시간 복잡도는 O(V+E), 공간 복잡도는 O(V)이입니다. 그래프의 모든
+ * 큐에 넣으며 진행합니다. 그래프의 경우 트리와 달리 노드가 순환하기 때문에
+ * 같은 노드를 방문하지 않도록 불 배열을 사용합니다.
+ * 
+ * 시간 복잡도는 O(V+E), 공간 복잡도는 O(V)입니다. 그래프의 모든
  * 정점을 탐색하는 데 적합하지만, 노드 수가 많을수록 탐색 범위가 선형적으로
  * 증가하여 성능 저하가 발생할 수 있습니다.
  *
@@ -20,15 +21,15 @@
 using namespace std;
 
 /**
- * 시작점이 주어진 BFS 탐색 알고리즘
+ * 시작점이 주어진 BFS
  * @param graph 탐색할 인접 리스트 그래프
  * @param start_node 탐색 시작 노드
- * @return 방문한 노드가 순서대로 저장
+ * @return 탐색한 노드들의 순서
  */
 vector<int> bfs_from_source(const vector<vector<int>> &graph, int start_node)
 {
     // 방문한 노드들을 저장할 벡터
-    vector<int> traversal_result;
+    vector<int> traverse_result;
 
     // 그래프의 정점 수
     int vertex_size = graph.size();
@@ -49,7 +50,7 @@ vector<int> bfs_from_source(const vector<vector<int>> &graph, int start_node)
         node_to_visit.pop();
 
         // 현재 노드를 방문 결과 기록
-        traversal_result.push_back(current_node);
+        traverse_result.push_back(current_node);
 
         // 현재 노드의 이웃 노드 탐색
         for (int neighbor_node : graph[current_node])
@@ -63,18 +64,18 @@ vector<int> bfs_from_source(const vector<vector<int>> &graph, int start_node)
         }
     }
 
-    return traversal_result;
+    return traverse_result;
 }
 
 /**
- * 모든 노드가 연결되어 있지 않는 그래프의 BFS 탐색 알고리즘
+ * 비연결 그래프의 BFS
  * @param graph 탐색할 인접 리스트 그래프
- * @return 방문한 노드가 순서대로 저장
+ * @return 탐색한 노드들의 순서
  */
 vector<int> dfs_disconnected(vector<vector<int>> &graph)
 {
     // 방문한 노드들을 저장할 벡터
-    vector<int> traversal_result;
+    vector<int> traverse_result;
 
     // 그래프의 정점 수
     int vertex_size = graph.size();
@@ -100,7 +101,7 @@ vector<int> dfs_disconnected(vector<vector<int>> &graph)
                 node_to_visit.pop();
 
                 // 현재 노드를 방문 결과 기록
-                traversal_result.push_back(current_node);
+                traverse_result.push_back(current_node);
 
                 // 현재 노드의 이웃 노드 탐색
                 for (int neighbor_node : graph[current_node])
@@ -116,7 +117,19 @@ vector<int> dfs_disconnected(vector<vector<int>> &graph)
         }
     }
 
-    return traversal_result;
+    return traverse_result;
+}
+
+/**
+ * 벡터 출력 함수
+ */
+void print_vector(const vector<int> vec) 
+{
+    for (int i = 0; i < vec.size(); i++) 
+    {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
 }
 
 int main(void)
@@ -131,15 +144,11 @@ int main(void)
         {2},        // 5
         {2}         // 6
     };
-
+    
     auto result1 = bfs_from_source(graph_connected, 3);
-
+    
     cout << "연결된 그래프의 탐색: ";
-
-    for (int n : result1)
-    {
-        cout << n << " ";
-    }
+    print_vector(result1);
 
     vector<vector<int>> graph_disconnected =
     {
@@ -155,28 +164,16 @@ int main(void)
         {9},       // 9(단독 연결)
         {8}        // 10(단독 연결)
     };
-
-    cout << "\n";
-
-    cout << "연결되지 않은 그래프의 탐색(bfs_from_source): ";
-
+    
     auto result2 = bfs_from_source(graph_disconnected, 0);
-
-    for (int n : result2)
-    {
-        cout << n << " ";
-    }
-
-    cout << "\n";
-
-    cout << "연결되지 않은 그래프의 탐색(graph_disconnected): ";
+    
+    cout << "연결되지 않은 그래프의 탐색(bfs_from_source): ";
+    print_vector(result2);
 
     auto result3 = dfs_disconnected(graph_disconnected);
-
-    for (int n : result3)
-    {
-        cout << n << " ";
-    }
+    
+    cout << "연결되지 않은 그래프의 탐색(dfs_disconnected): ";
+    print_vector(result3);
 
     return 0;
 }
