10c41cd41591c01785149420ea07237c47734254
김주영 <98079486+Leusin@users.noreply.github.com>
2025-05-15 00:12:35 +0900

    [추가] STL 의 주요 알고리즘 예제
    - count: 일치하는 값 횟수 세기
    - sort: 함수로 정렬하기
    - next_permutation: 순열 생성하기
    - unique: 중복값 요소 제거
    - max_element / min_element

---
+++ Algorithms/STLAlgorithm.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Point
+{
+    int x, y;
+    Point(int x, int y) : x(x), y(y) {}
+};
+
+bool compare(const Point& a, const Point& b)
+{
+    if(a.x == b.x)
+    {
+        return a.y < b.y;
+    }
+
+    return a.x < b.x;
+}
+
+int main()
+{
+    vector<int> v1 = {2, 1, 3, 3};
+    vector<Point> v2 = {{3, 4}, {1, 2}, {3, 5}};
+
+    /**
+     * 일치하는 값 횟수 세기
+     */
+    int count_result = count(v1.begin(), v1.end(), 3); // 3 값이 컨테이너에 몇 번 나타나는지 세기 (2)
+
+    /**
+     * 함수로 정렬하기
+     */
+    sort(v1.begin(), v1.end()); // 오름차순으로 정렬 (1, 2, 3, 3)
+    sort(v1.rbegin(), v1.rend()); // 내림차순으로 정렬 (3, 3, 2, 1)
+
+    sort(v2.begin(), v2.end(), compare); // 사용자 비교 함수를 통해 오름차순으로 정렬 ((1, 2), (3, 4), (3, 5))
+
+    /**
+     * 가능한 모든 순열 생성하기
+     */
+    sort(v1.begin(), v1.end()); // 순열을 생성하기 전에 대이터가 사전 순으로 정렬된 상태야 함.
+    // 중복 값이 포함된 컨테이너라도 next_permutation은 중복되지 않은 순열만 생성 (O(N*N!))
+    do
+    {
+        for(int i: v1)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+    while(next_permutation(v1.begin(), v1.end()));
+    /*
+    출력값:
+    1 2 3 3
+    1 3 2 3
+    1 3 3 2
+    2 1 3 3
+    2 3 1 3
+    2 3 3 1
+    3 1 2 3
+    3 1 3 2
+    3 2 1 3
+    3 2 3 1
+    3 3 1 2
+    3 3 2 1
+    */
+
+    /**
+     * 중복값 요소 제거
+     */
+    
+     // 중복되는 요소를 뒤로 밀어낸 뒤 중복되지 않는 새로운 범위의 끝 반복자를 반환(O(n))
+    auto new_end = unique(v1.begin(), v1.end()); 
+
+    // 중복되지 않은 요소들만 출력 (1, 2, 3)
+    for(auto it = v1.begin(); it != new_end; it++)
+    {
+        cout << *it << " ";
+    }
+
+    sort(v1.begin(), v1.end()); // 이미 정렬되어 있어야 정상 작동
+
+    /**
+     * 최댓값, 최소값 위치 구하기기
+     */
+    auto max_it = max_element(v1.begin(), v1.end());
+    auto min_it = min_element(v1.begin(), v1.end());
+    
+    return 0;
+}
