a33062f87254fcf411fd178d93fd43772bed347e
김주영 <98079486+Leusin@users.noreply.github.com>
2025-05-17 18:06:34 +0900

    [추가] STL 컨테이너 맵 예제

--- Algorithms/STLAlgorithm.cpp
+++ Algorithms/STLAlgorithm.cpp
@@ -41,7 +41,7 @@ int main()
     /**
      * 가능한 모든 순열 생성하기
      */
-    sort(v1.begin(), v1.end()); // 순열을 생성하기 전에 대이터가 사전 순으로 정렬된 상태야 함.
+    sort(v1.begin(), v1.end()); // 순열을 생성하기 전에 데이터가 사전 순으로 정렬된 상태여야 함.
     // 중복 값이 포함된 컨테이너라도 next_permutation은 중복되지 않은 순열만 생성 (O(N*N!))
     do
     {
@@ -53,41 +53,42 @@ int main()
     }
     while(next_permutation(v1.begin(), v1.end()));
     /*
-    출력값:
-    1 2 3 3
-    1 3 2 3
-    1 3 3 2
-    2 1 3 3
-    2 3 1 3
-    2 3 3 1
-    3 1 2 3
-    3 1 3 2
-    3 2 1 3
-    3 2 3 1
-    3 3 1 2
-    3 3 2 1
+        출력값:
+        1 2 3 3
+        1 3 2 3
+        1 3 3 2
+        2 1 3 3
+        2 3 1 3
+        2 3 3 1
+        3 1 2 3
+        3 1 3 2
+        3 2 1 3
+        3 2 3 1
+        3 3 1 2
+        3 3 2 1
     */
 
     /**
      * 중복값 요소 제거
      */
-    
-     // 중복되는 요소를 뒤로 밀어낸 뒤 중복되지 않는 새로운 범위의 끝 반복자를 반환(O(n))
-    auto new_end = unique(v1.begin(), v1.end()); 
+    sort(v1.begin(), v1.end()); // 반드시 정렬되어 있어야 정상 작동
 
-    // 중복되지 않은 요소들만 출력 (1, 2, 3)
-    for(auto it = v1.begin(); it != new_end; it++)
-    {
-        cout << *it << " ";
-    }
+    auto new_end = unique(v1.begin(), v1.end()); // 중복되는 요소를 뒤로 밀어낸 뒤 중복되지 않는 새로운 범위의 끝 반복자를 반환(O(n))
 
-    sort(v1.begin(), v1.end()); // 이미 정렬되어 있어야 정상 작동
+    v1.erase(new_end, v1.end());
 
     /**
-     * 최댓값, 최소값 위치 구하기기
+     * 이진 탐색하기
      */
-    auto max_it = max_element(v1.begin(), v1.end());
-    auto min_it = min_element(v1.begin(), v1.end());
-    
+    sort(v1.begin(), v1.end()); // 반드시 정렬되어 있어야 정상 작동
+
+    bool contain = binary_search(v1.begin(), v1.end(), 3); // 값이 있다면 1 없다면 0 반환
+
+    /**
+     * 최댓값, 최소값 위치 구하기
+     */
+    auto max_it = max_element(v1.begin(), v1.end()); // 최대값 위치를 가르키는 반복자
+    auto min_it = min_element(v1.begin(), v1.end()); // 최소값 위치를 가르키는 반복자
+
     return 0;
 }
--- DataStructures/Built-int/STLString.cpp
+++ DataStructures/Built-int/STLString.cpp
@@ -11,29 +11,29 @@ int main()
     /**
      * 문자열 선언 및 초기화
      */
-    string str1;                   // 빈 문자열 선언
-    string str2 = "Hello, World!"; // 직접 초기화
-    string str3(str2);             // 복사 초기화
-    string str4(str2, 0, 5);       // 부분 복사 초기화 ("Hello")
-    string str5(8, '*');           // 반복된 문자 초기화 ("********")
+    string str;                    // 빈 문자열 선언
+    string str1 = "Hello, World!"; // 직접 초기화
+    string str2(str1);             // 복사 초기화
+    string str3(str1, 0, 5);       // 부분 복사 초기화 ("Hello")
+    string str4(8, '*');           // 반복된 문자 초기화 ("********")
 
     /**
      * 문자열 찾기
      */
-    size_t pos1 = str2.find("Hello"); // 문자열 찾기 (0)
-    size_t pos2 = str2.find('W');     // 문자 찾기 (7)
+    size_t pos1 = str1.find("Hello"); // 문자열 찾기 (0)
+    size_t pos2 = str1.find('W');     // 문자 찾기 (7)
 
-    size_t pos3 = str2.find("Hello", 2); // 2번째 자리부터 문자열 찾기 (18446744073709551615; string::npos)
+    size_t pos3 = str1.find("Hello", 2); // 2번째 자리부터 문자열 찾기 (18446744073709551615; string::npos)
 
     /**
      * 문자열 추가와 수정
      */
-    string str6 = "Hi";
-    str6 += ", World!"; // 문자열 추가 ("Hi, World!")
+    string str5 = "Hi";
+    str5 += ", World!"; // 문자열 추가 ("Hi, World!")
 
-    str6[1] = 'e'; // 1번째 문자 수정 'i' → 'e' ("He, World!")
-    str6[2] = 'y'; // 2번째 문자 수정',' → 'y' ("Hey World!")
+    str5[1] = 'e'; // 1번째 문자 수정 'i' → 'e' ("He, World!")
+    str5[2] = 'y'; // 2번째 문자 수정',' → 'y' ("Hey World!")
     
-    str6.replace(4, 5, "Apple"); // 인덱스 4부터 5글자를 "Apple"로 대체
+    str5.replace(4, 5, "Apple"); // 인덱스 4부터 5글자를 "Apple"로 대체
 
 }
